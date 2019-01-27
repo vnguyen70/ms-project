bd1820a80cc0804c4750dd1ea119cbb9c180438e
vnguyen70 <nguyen.vi@gatech.edu>
2019-01-27 16:11:24 -0500

    code cleanup
    moved text content from MainDialogue.js to separate files

---
+++ src/Components/Summary/summaryText.js
@@ -0,0 +1,86 @@
+export const summaryText = [
+  {
+    id: "m0-nice",
+    text:
+      "Based on your responses, your personality has been established as 'Nice.' Sam will feel more comfortable opening up to you without fear of judgment. You will start the game with 3 affection hearts."
+  },
+  {
+    id: "m0-silly",
+    text:
+      "Based on your responses, your personality has been established as 'Silly.' Sam will have mixed feelings opening up to you. Knowing when to be serious and when to be playful will be key for gaining her trust. You will start the game with 2 affection hearts."
+  },
+  {
+    id: "m0-sarcastic",
+    text:
+      "Based on your responses, your personality has been established as 'Sarcastic.' Sam will feel more reserved when speaking to you due to your borderline mean nature. Being nicer is key to gaining her trust. You will start the game with 1 affection hearts."
+  },
+  {
+    id: "m0-default",
+    text:
+      "Great job, you've completed the first month of iComforter! From here on out, time will pass by in 6 month intervals. You will start off with 3 affection hearts."
+  },
+
+  {
+    id: "m6-end1",
+    text:
+      "You asked Sam why she's sad, but in a way that makes her feel bad about her own feelings. She feels as though you're judging her for feeling this way, and she does not feel comfortable to continue the conversation. You and Sam share some small chit-chat for the next few days, but you don't hear from Sam after that. <br/>You failed to help Sam. Next time, you should offer your support and ask her to open up about how she feels without belittling her. <br/>Game over."
+  },
+  {
+    id: "m6-end2",
+    text:
+      "You did not prompt Sam to open up about her feelings. She feels as though your comment was just empty words, and does not feel your concern. You and Sam share some small chit-chat for the next few days, but you don't hear from Sam after that. <br/>You failed to help Sam. Next time, you should offer your support and ask her to open up about how she feels. <br/>Game over."
+  },
+  {
+    id: "m6-good",
+    text:
+      "You asked Sam what's wrong. Good job! She may not have spoken to anyone about her concerns, so it was the right choice for you to get her to open up and talk about how she feels. It was also good that you asked her about her suicidal comment, as this is something you should always take seriously. You and/or Sam should contact her general practioner and inform them that she is having suicidal thoughts."
+  },
+  {
+    id: "m6-ok",
+    text:
+      "You asked Sam what's wrong. Good job! She may not have spoken to anyone about her concerns, so it was the right choice for you to get her to open up and talk about how she feels. Be cautious of when she talks about 'sleeping forever' as this is a red flag for suicide. You and/or Sam should contact her general practioner and inform them that she is having suicidal thoughts."
+  },
+
+  {
+    id: "m12-end1",
+    text:
+      "You did not properly encourage Sam, and have let her succumb to her own demotivation due to her depression. You and Sam share some small chit-chat for the next few days, but you don't hear from Sam after that. <br/>You failed to help Sam. Next time, you should be kinder in your words and push her to overcome her letharagy. <br/>Game over. "
+  },
+  {
+    id: "m12-good",
+    text:
+      "You encouraged Sam. Good job! Allowing Sam to give up here would not help her out in the long-run, so it's good that you motivated her to not fall behind on her applications. <br/>Also, great job encouraging her in a kind and gentle manner! Using kind, non-threatening vocabulary is key to fostering a safe environment for Sam to open up to you. By giving Sam gentle encouragement and suggesting ways of destressing like taking breaks or working with a friend, this helps Sam help herself."
+  },
+  {
+    id: "m12-better",
+    text:
+      "You encouraged Sam. Good job! Allowing Sam to give up here would not help her out in the long-run, so it's good that you motivated her to not fall behind on her applications. <br/>Remember to continue using kind, non-threatening vocabulary to foster a safe environment for Sam to open up to you. By giving Sam gentle encouragement and suggesting ways of destressing like taking breaks or working with a friend, this helps Sam help herself."
+  },
+  {
+    id: "m12-ok",
+    text:
+      "You encouraged Sam. Good job! Allowing Sam to give up here would not help her out in the long-run, so it's good that you motivated her to not fall behind on her applications. <br/>However, Sam might have taken offense by the way you encouraged her. Remember to use kind, non-threatening vocabulary to foster a safe environment for Sam to open up to you. By giving Sam gentle encouragement and suggesting ways of destressing like taking breaks or working with a friend, this helps Sam help herself."
+  },
+
+  {
+    id: "m18-end1",
+    text:
+      "You did not suggest professional help to Sam. Although your support has been helping her recently, it's crucial that she seeks out professional help as well. Your chats with her are starting to cause you emotional strain, so you shouldn't ignore your own feelings just because you want to help your friend. It is important to establish emotional boundaries with a depressed friend so you can help them while also helping yourself. <br/>You and Sam repeat the same type of conversation over and over for the next few weeks before the conversation sizzles out and you two stop talking. <br/>Game over."
+  },
+  {
+    id: "m18-end2",
+    text:
+      "You lashed out and failed to establish emotional boundaries with Sam. Although establishing emotional boundaries are important, this was not the way to do so. There is a difference between establishing boundaries while providing support and simply driving the depressed person away. In this case, you did the latter. <br/>You failed to help Sam. She no longer feels comfortable talking to you about her problems. <br/>Game over. "
+  },
+
+  {
+    id: "m24-BadEnding",
+    text:
+      "You failed to help Sam. Through your enabling behavior, Sam was not able to seek out professional help. You also did not set your emotional boundaries with her. Although you meant well, your actions did not help Sam in the long-run. "
+  },
+  {
+    id: "m24-GoodEnding",
+    text:
+      "You've helped Sam! Throughout this experience, you have managed to suggest professional help, establish emotional boundaries and offer your support to your depressed friend. Hopefully you have learned a thing or two about how to go about helping a person with depression while also maintaining your own health. <br/>Congrats! Feel free to try out different paths."
+  }
+];
